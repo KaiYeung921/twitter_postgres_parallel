6391af3df921f0d6f74fe0bcc469d0bb737ab6c7
Kai <kyeung921@gmail.com>
2026-04-09 15:17:02 -0700

    db fix

--- load_tweets_sequential.sh
+++ load_tweets_sequential.sh
@@ -1,32 +1,19 @@
-#!/bin/bash
+#!/bin/sh
 
 files=$(find data/*)
 
 echo '================================================================================'
-echo 'load denormalized'
+echo 'load pg_denormalized'
 echo '================================================================================'
-time for file in $files; do
-    echo
-    # copy your solution to the twitter_postgres assignment here
-    python3 load_tweets.py \
-    --db postgresql://postgres:pass@localhost:7272/postgres \
-    --inputs $file
-done
+echo "$files" | parallel sh load_denormalized.sh {}
 
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
-time for file in $files; do
-    echo
-    # copy your solution to the twitter_postgres assignment here
-    python3 load_tweets.py \
-    --db postgresql://postgres:pass@localhost:7564/postgres \
-    --inputs $file
-done
+echo "$files" | parallel python3 load_tweets.py --db=postgresql://postgres:pass@localhost:7564/postgres --inputs {}
 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
-time for file in $files; do
-    python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:7373/postgres --inputs $file
-done
+echo "$files" | parallel python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:7373/postgres --inputs {}
+
